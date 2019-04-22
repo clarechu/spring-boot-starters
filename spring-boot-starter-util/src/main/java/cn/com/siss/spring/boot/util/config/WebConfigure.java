22aae3ad1bd52a82cca6112fe5e6f71d36df1d17
ClareChu <1062186165@qq.com>
2019-04-22 15:11:46 +0800

    web configure

--- spring-boot-starter-util/src/main/java/cn/com/siss/spring/boot/util/config/WebConfigure.java
+++ spring-boot-starter-util/src/main/java/cn/com/siss/spring/boot/util/config/WebConfigure.java
@@ -6,20 +6,15 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-@EnableWebMvc
+//@EnableWebMvc
 @Configuration
 public class WebConfigure extends WebMvcConfigurerAdapter {
 
@@ -27,28 +22,12 @@ public class WebConfigure extends WebMvcConfigurerAdapter {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(serializeResponseBody());
     }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-    @Bean
-    public ViewResolver iRViewResolver(){
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        //设置优先级，值越低，优先级越高
-        //让所有处理不了的VieResolver最后都交给InternalResourceViewResolver处理
-        resolver.setOrder(Ordered.LOWEST_PRECEDENCE);
-        return resolver;
-    }
-
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //指定了静态资源文件的位置
-        registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/resources/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Bean
