8c77ae5f73f7782114813e21f61af4b809bff1ce
ClareChu <1062186165@qq.com>
2019-04-26 14:14:01 +0800

    log outputstring exception

--- spring-boot-starter-util/src/main/java/cn/com/siss/spring/boot/util/controller/ExceptionConfigController.java
+++ spring-boot-starter-util/src/main/java/cn/com/siss/spring/boot/util/controller/ExceptionConfigController.java
@@ -4,6 +4,7 @@ import cn.com.siss.spring.boot.util.base.BackResponseUtil;
 import cn.com.siss.spring.boot.util.base.BaseResponse;
 import cn.com.siss.spring.boot.util.base.ReturnCodeEnum;
 import cn.com.siss.spring.boot.validate.exception.DataException;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,10 +19,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 @ResponseBody
+//@Configurable
 public class ExceptionConfigController {
 
     @ExceptionHandler(NullPointerException.class)
     public BaseResponse nullPointerExceptionHandler(NullPointerException e){
+        e.printStackTrace();
         String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_NULL_POINTER_EXCEPTION.getCode());
         baseResponse.setData(message);
@@ -30,6 +33,7 @@ public class ExceptionConfigController {
 
     @ExceptionHandler(RuntimeException.class)
     public BaseResponse runtimeException(RuntimeException e){
+        e.printStackTrace();
         String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_RUNTIME_EXCEPTION.getCode());
         baseResponse.setData(message);
@@ -38,6 +42,7 @@ public class ExceptionConfigController {
 
     @ExceptionHandler(Exception.class)
     public BaseResponse exception(Exception e){
+        e.printStackTrace();
         String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_EXCEPTION.getCode());
         baseResponse.setData(message);
@@ -46,6 +51,7 @@ public class ExceptionConfigController {
 
     @ExceptionHandler(DataException.class)
     public BaseResponse dataException(Exception e){
+        e.printStackTrace();
         String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_PAYMENT_TYPE_ERROR.getCode());
         baseResponse.setData(message);
