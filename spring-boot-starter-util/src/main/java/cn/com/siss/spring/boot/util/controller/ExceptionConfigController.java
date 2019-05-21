d8c907d7a185b128f77cc6412cfb2fe16a8aea32
xufeng <296800108@qq.com>
2019-05-21 16:33:38 +0800

    bug修复

--- spring-boot-starter-util/src/main/java/cn/com/siss/spring/boot/util/controller/ExceptionConfigController.java
+++ spring-boot-starter-util/src/main/java/cn/com/siss/spring/boot/util/controller/ExceptionConfigController.java
@@ -3,6 +3,7 @@ package cn.com.siss.spring.boot.util.controller;
 import cn.com.siss.spring.boot.util.base.BackResponseUtil;
 import cn.com.siss.spring.boot.util.base.BaseResponse;
 import cn.com.siss.spring.boot.util.base.ReturnCodeEnum;
+import cn.com.siss.spring.boot.util.other.StringUtil;
 import cn.com.siss.spring.boot.validate.exception.DataException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -10,6 +11,10 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ClassName ExceptionConfigController
@@ -24,39 +29,53 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 public class ExceptionConfigController {
 
+    private static Map<String,String> messageMap=new HashMap<>();
+
+    @PostConstruct
+    private void init(){
+        messageMap.put("message.common.success","成功");
+        messageMap.put("message.common.failed","失败");
+        messageMap.put("message.common.information.nonexistence","信息不存在");
+        messageMap.put("message.payment.type.error","支付方式错误");
+        messageMap.put("message.common.server.error","服务器处理异常");
+        messageMap.put("message.common.parameters.missing","参数不全");
+        messageMap.put("message.common.timeout","请求超时");
+        messageMap.put("message.common.information.already.exists","信息已存在");
+        messageMap.put("message.common.session.expired","登录状态失效");
+        messageMap.put("message.security.auth.failed","验证失败");
+        messageMap.put("message.security.unauthorized","无权限访问");
+        messageMap.put("message.common.insert.failed","插入失败");
+        messageMap.put("message.common.delete.failed","删除失败");
+    }
+
     @ExceptionHandler(NullPointerException.class)
     public BaseResponse nullPointerExceptionHandler(NullPointerException e){
         log.error(e.getMessage(),e);
-        String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_NULL_POINTER_EXCEPTION.getCode());
-        baseResponse.setMessage(message);
+        baseResponse.setMessage(getMessage(e));
         return baseResponse;
     }
 
     @ExceptionHandler(RuntimeException.class)
     public BaseResponse runtimeException(RuntimeException e){
         log.error(e.getMessage(),e);
-        String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_RUNTIME_EXCEPTION.getCode());
-        baseResponse.setMessage(message);
+        baseResponse.setMessage(getMessage(e));
         return baseResponse;
     }
 
     @ExceptionHandler(Exception.class)
     public BaseResponse exception(Exception e){
         log.error(e.getMessage(),e);
-        String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_EXCEPTION.getCode());
-        baseResponse.setMessage(message);
+        baseResponse.setMessage(getMessage(e));
         return baseResponse;
     }
 
     @ExceptionHandler(DataException.class)
     public BaseResponse dataException(Exception e){
-        log.error(e.getMessage(),e);
-        String message = e.getMessage();
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_PAYMENT_TYPE_ERROR.getCode());
-        baseResponse.setMessage(message);
+        baseResponse.setMessage(getMessage(e));
         return baseResponse;
     }
 
@@ -66,6 +85,15 @@ public class ExceptionConfigController {
         BaseResponse baseResponse = BackResponseUtil.setBaseResponse(ReturnCodeEnum.MESSAGE_EXCEPTION.getCode());
         baseResponse.setMessage(e.getAllErrors().get(0).getDefaultMessage());
         return baseResponse;
+    }
+
+    private String getMessage(Exception e){
+        String msg=e.getMessage();
+        String message=messageMap.get(msg);
+        if (StringUtil.isNotEmpty(message)){
+            return message;
+        }
+        return msg;
     }
 
 }
